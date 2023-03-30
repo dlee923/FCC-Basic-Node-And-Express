b2fb11ac854280462d61fa0ae73ec9f70399943d
Daniel Lee <dlee923@gmail.com>
2023-03-30 12:47:09 -0400

    test

--- myApp.js
+++ myApp.js
@@ -16,9 +16,11 @@ app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', function(req, res) {
     if (process.env.MESSAGE_STYLE === "uppercase") {
-        res.json({"message": "HELLO JSON"});
-    } else {
+        // res.json({"message": "HELLO JSON"});
         res.json({"message": "Hello json"});
+    } else {
+        // res.json({"message": "Hello json"});
+        res.json({"message": "HELLO JSON"});
     }    
 });
 
