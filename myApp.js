f021348c4abb871f7d495b733df2ee3049cc62f6
Replit user <>
2023-03-30 17:09:29 +0000

    added secret

--- myApp.js
+++ myApp.js
@@ -10,7 +10,7 @@ app.get('/', function(req, res) {
     res.sendFile(absolutePath);
 });
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/git public'));
 
 
 
