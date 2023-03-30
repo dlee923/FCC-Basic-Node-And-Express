561a3bd354fc8d3253b8c22667e293673b99ce5e
Daniel Lee <dlee923@gmail.com>
2023-03-30 12:54:26 -0400

    reverted back - added env variable to replit as secrets variable as git ignores the .env file

--- myApp.js
+++ myApp.js
@@ -18,7 +18,7 @@ app.get('/json', function(req, res) {
     if (process.env.MESSAGE_STYLE === "uppercase") {
         res.json({"message": "HELLO JSON"});
     } else {
-        res.json({"message": process.env.MESSAGE_STYLE});
+        res.json({"message": "Hello Json"});
     }    
 });
 
