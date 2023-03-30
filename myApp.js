22cb72602b2ee5f19ec2b18c6b54fd2bbb87959d
Daniel Lee <dlee923@gmail.com>
2023-03-30 14:18:58 -0400

    commenting out

--- myApp.js
+++ myApp.js
@@ -4,31 +4,31 @@ require('dotenv').config();
 
 console.log("Hello World");
 
-// to be able to install the html file
-app.get('/', function(req, res) {
-    // res.send("Hello Express");
-    let absolutePath = __dirname + "/views/index.html";
-    res.sendFile(absolutePath);
-});
+// // to be able to install the html file
+// app.get('/', function(req, res) {
+//     // res.send("Hello Express");
+//     let absolutePath = __dirname + "/views/index.html";
+//     res.sendFile(absolutePath);
+// });
 
-// to MOUNT the CSS file in the public folder
-app.use('/public', express.static(__dirname + '/public'));
+// // to MOUNT the CSS file in the public folder
+// app.use('/public', express.static(__dirname + '/public'));
 
-// to apply .env variables
-app.get('/json', function(req, res) {
-    let response = "Hello Json"
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        response = response.toUpperCase();
-        res.json({'message': response});
-    } else {
-        res.json({'message': response});
-    }
-});
+// // to apply .env variables
+// app.get('/json', function(req, res) {
+//     let response = "Hello Json"
+//     if (process.env.MESSAGE_STYLE === "uppercase") {
+//         response = response.toUpperCase();
+//         res.json({'message': response});
+//     } else {
+//         res.json({'message': response});
+//     }
+// });
 
-app.use(function(req, res, next) {
-    console.log(req.method + " " + req.path + " - " + req.ip);
-    next();
-});
+// app.use(function(req, res, next) {
+//     console.log(req.method + " " + req.path + " - " + req.ip);
+//     next();
+// });
 
 app.get('/now', function(req, res) {
     req.time = new Date().toString();
